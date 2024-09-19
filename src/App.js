f0fb018fd85b46440f5ec53bc3b559a2a4ac3437
manixcoder <pathakmanish86@gmail.com>
2024-09-20 01:17:34 +0530

    Routing code update

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ export default class App extends Component {
         <Router>
           <Navbar />
           <Routes>
-            <Route path='/' element={<News key='general' pageSize={5} country='us' category='general' />}></Route>
-            <Route path='/business' element={<News key='business' pageSize={5} country='us' category='business' />}></Route>
-            <Route path='/entertainment' element={<News key='entertainment' pageSize={5} country='us' category='entertainment' />}></Route>
-            <Route path='/health' element={<News key='health' pageSize={5} country='us' category='health' />}></Route>
-            <Route path='/science' element={<News key='science' pageSize={5} country='us' category='science' />}></Route>
-            <Route path='/sports' element={<News key='sports' pageSize={5} country='us' category='sports' />}></Route>
-            <Route path='/technology' element={<News key='technology' pageSize={5} country='us' category='technology' />}></Route>
+            <Route exact path='/' element={<News key='general' pageSize={5} country='us' category='general' />}></Route>
+            <Route exact path='/business' element={<News key='business' pageSize={5} country='us' category='business' />}></Route>
+            <Route exact path='/entertainment' element={<News key='entertainment' pageSize={5} country='us' category='entertainment' />}></Route>
+            <Route exact path='/health' element={<News key='health' pageSize={5} country='us' category='health' />}></Route>
+            <Route exact path='/science' element={<News key='science' pageSize={5} country='us' category='science' />}></Route>
+            <Route exact path='/sports' element={<News key='sports' pageSize={5} country='us' category='sports' />}></Route>
+            <Route exact path='/technology' element={<News key='technology' pageSize={5} country='us' category='technology' />}></Route>
           </Routes>
         </Router>
       </div>
