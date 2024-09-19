882668efd37df17a0ad738104c2d968791f5f167
manixcoder <pathakmanish86@gmail.com>
2024-09-19 18:52:31 +0530

    Routing code

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <Navbar />
-        <News pageSize={5} country="us" category="science"/>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<News key='general' pageSize={5} country='us' category='general' />}></Route>
+            <Route path="/" element = {<News pageSize = {5} country = "in" category = "general1"/>}/>
+            <Route path='/entertainment' element={<News key='entertainment' pageSize={5} country='us' category='entertainment' />}></Route>
+            <Route path='/health' element={<News key='health' pageSize={5} country='us' category='health' />}></Route>
+            <Route path='/science' element={<News key='science' pageSize={5} country='us' category='science' />}></Route>
+            <Route path='/sports' element={<News key='sports' pageSize={5} country='us' category='sports' />}></Route>
+            <Route path='/technology' element={<News key='technology' pageSize={5} country='us' category='technology' />}></Route>
+          </Routes>
+        </Router>
       </div>
     )
   }
